7486ae5f2888ff4746e639661ceeda5378cb724d
zhangzc_official <zzc459941818@gmail.com>
2026-05-28 17:23:00 +0800

    feat: DeskItem 掉出屏幕下方自动销毁

--- scripts/test/desk_item.gd
+++ scripts/test/desk_item.gd
@@ -1,6 +1,10 @@
 class_name DeskItem
 extends RigidBody2D
 
+## 桌面物品（物理体）。掉出屏幕下方自动销毁。
+
+const KILL_Y: float = 800.0
+
 var _pending_color: Color = Color.WHITE
 
 @onready var _visual: Polygon2D = $Visual
@@ -8,6 +12,11 @@ var _pending_color: Color = Color.WHITE
 
 func _ready() -> void:
 	_visual.color = _pending_color
+
+
+func _physics_process(_delta: float) -> void:
+	if global_position.y > KILL_Y:
+		queue_free()
 
 
 func set_color(c: Color) -> void:
